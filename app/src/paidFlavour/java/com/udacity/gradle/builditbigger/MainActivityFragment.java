f4b00617bfb13c6e33ad1883e8ca347cf215a723
Mohamed AbdelraZek <muhamedrabi3@gmail.com>
2017-04-13 13:26:16 +0200

    adding paid and free flavours

--- app/src/paidFlavour/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paidFlavour/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,5 +1,4 @@
 package com.udacity.gradle.builditbigger;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -14,8 +13,6 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.Joker;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
@@ -32,7 +29,7 @@ import mohamedabdelrazek.com.androidjokeslib.JokesActivity;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
- private    Button button;
+    private Button button;
     private ProgressBar mProgressBar;
 
     public MainActivityFragment() {
@@ -44,15 +41,6 @@ public class MainActivityFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
         button = (Button) root.findViewById(R.id.tell_joke_btn);
         mProgressBar= (ProgressBar) root.findViewById(R.id.progressBar);
-
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
